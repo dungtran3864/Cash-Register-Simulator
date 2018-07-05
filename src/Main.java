970b096f8d7c72675b364f6af1cd1597495e00aa
Dung <dxt6193@rit.edu>
2018-07-04 20:48:59 -0400

    Total cost and paid amount results in expected change.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter total cost: ");
+        System.out.print("Enter total cost: ");
         double cost = scanner.nextDouble();
         cost = Math.round(cost * 100.0) / 100.0;
-        System.out.println("Enter paid amount: ");
+        System.out.print("Enter paid amount: ");
         double paid = scanner.nextDouble();
         paid = Math.round(paid * 100.0) / 100.0;
         scanner.close();
