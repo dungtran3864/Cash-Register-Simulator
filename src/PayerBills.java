4206edf2b2f6b7e5c987b2191b484bae262a08e2
Dung <dxt6193@rit.edu>
2018-09-15 11:12:37 -0400

    Improve the user input experience.

--- src/PayerBills.java
+++ src/PayerBills.java
@@ -12,63 +12,23 @@ public class PayerBills {
     }
 
     public void initializePayerBills() {
-        int billsNumber;
+
+        System.out.println("Input in the bills and number of each bill the following form: bill1 amount1, bill2 amount2, etc");
+        System.out.println("Failure to follow the form correctly will result in a failed transaction and starting over");
+        System.out.print("Now type in your bills: ");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("$100 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 100.0 * billsNumber;
-        billsCounter.put("Hundred", billsNumber);
+        String billsString = scanner.nextLine();
+        String[] billsList = billsString.split(",");
 
-        System.out.print("$50 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 50.0 * billsNumber;
-        billsCounter.put("Fifty", billsNumber);
-
-        System.out.print("$20 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 20.0 * billsNumber;
-        billsCounter.put("Twenty", billsNumber);
-
-        System.out.print("$10 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 10.0 * billsNumber;
-        billsCounter.put("Ten", billsNumber);
-
-        System.out.print("$5 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 5.0 * billsNumber;
-        billsCounter.put("Five", billsNumber);
-
-        System.out.print("$2 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 2.0 * billsNumber;
-        billsCounter.put("Two", billsNumber);
-
-        System.out.print("$1 bills given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 1.0 * billsNumber;
-        billsCounter.put("One", billsNumber);
-
-        System.out.print("Quarter given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 0.25 * billsNumber;
-        billsCounter.put("Quarter", billsNumber);
-
-        System.out.print("Dime given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 0.10 * billsNumber;
-        billsCounter.put("Dime", billsNumber);
-
-        System.out.print("Nickel given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 0.05 * billsNumber;
-        billsCounter.put("Nickel", billsNumber);
-
-        System.out.print("Penny given: ");
-        billsNumber = scanner.nextInt();
-        paid = paid + 0.01 * billsNumber;
-        billsCounter.put("Penny", billsNumber);
+        for (String itemString : billsList) {
+            String[] itemList = itemString.trim().split("\\s+");
+            String bill = itemList[0].toLowerCase();
+            bill = bill.substring(0,1).toUpperCase() + bill.substring(1).toLowerCase();
+            int amount = Integer.parseInt(itemList[1]);
+            addTotalPaid(bill, amount);
+            this.billsCounter.put(bill, amount);
+        }
 
         System.out.println("Total paid: " + this.paid);
 
@@ -76,6 +36,32 @@ public class PayerBills {
 
     public HashMap<String, Integer> getBillsCounter() {
         return billsCounter;
+    }
+
+    public void addTotalPaid(String bill, int amount) {
+        if (bill.equals("Hundred")) {
+            paid = paid + 100.0 * amount;
+        } else if (bill.equals("Fifty")) {
+            paid = paid + 50.0 * amount;
+        } else if (bill.equals("Twenty")) {
+            paid = paid + 20.0 * amount;
+        } else if (bill.equals("Ten")) {
+            paid = paid + 10.0 * amount;
+        } else  if (bill.equals("Five")) {
+            paid = paid + 5.0 * amount;
+        } else if (bill.equals("Two")) {
+            paid = paid + 2.0 * amount;
+        } else if (bill.equals("One")) {
+            paid = paid + 1.0 * amount;
+        } else if (bill.equals("Quarter")) {
+            paid = paid + 0.25 * amount;
+        } else if (bill.equals("Dime")) {
+            paid = paid + 0.10 * amount;
+        } else if (bill.equals("Nickel")) {
+            paid = paid + 0.05 * amount;
+        } else if (bill.equals("Penny")){
+            paid = paid + 0.01 * amount;
+        }
     }
 
     public double getPaid() {
